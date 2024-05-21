50a886e62b6a4ace109a695806dfdf927676d450
ValhallaAMB <abdomohaiminbashir@hotmail.com>
2024-05-21 05:13:34 +0300

    Removed useNewUrlParser

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-    });
+    const db = await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error(err.message);
